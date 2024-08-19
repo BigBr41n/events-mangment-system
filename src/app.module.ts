fdf083df5af0c72288d68fbbb19bcbe4bbe440da
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-19 02:52:32 +0200

    adding typeORM and config fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}` || '.env',
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development.local',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string()
           .valid('development', 'production')
@@ -15,8 +19,22 @@ import Joi from 'joi';
         PORT: Joi.number(),
       }),
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: process.env.NODE_ENV === 'development',
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+console.log('NODE_ENV:', process.env.NODE_ENV);
