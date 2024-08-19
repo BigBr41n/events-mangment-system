f3ccbb4de0d61e1a99cec9a8f0c1e89b9c00c457
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-19 02:12:19 +0200

    using fastify instead of express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCors from '@fastify/cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  //setting up cors and allow all origins
+  app.register(fastifyCors, {
+    origin: '*',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
