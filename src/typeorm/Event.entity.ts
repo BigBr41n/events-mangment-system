7c821fa16023f17e605c7e14c4bdbc171b4182cd
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-19 17:59:48 +0200

    db: Event entity

---
+++ src/typeorm/Event.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany, CreateDateColumn, UpdateDateColumn, Index } from 'typeorm';
+
+@Entity('events')
+@Index('IDX_EVENTS_ORGANIZER_ID', ['organizerId'])
+export class Event {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  title: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'timestamp' })
+  startDate: Date;
+
+  @Column({ type: 'timestamp' })
+  endDate: Date;
+
+  @Column({ type: 'varchar', length: 255 })
+  location: string;
+
+  @Column({ type: 'int' })
+  capacity: number;
+
+  @Column({ type: 'uuid' })
+  organizerId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
