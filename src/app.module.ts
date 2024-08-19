848b1420f8a647ffb74766d1b197ff9e9e30b462
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-19 02:21:15 +0200

    config and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}` || '.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DATABASE_URL: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
