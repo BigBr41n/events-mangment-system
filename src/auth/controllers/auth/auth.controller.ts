6b87cd50c4287377ff235121297309eade163699
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-20 21:04:58 +0200

    auth controller

---
+++ src/auth/controllers/auth/auth.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Body,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { LocalAuthGuard } from '../../guards/LocalAuthGuard.guard';
+import { AuthService } from '../../services/auth/auth.service';
+import { CreateUserDto } from '../../dtos/CreateUser.dto';
+import { Request } from '@nestjs/common';
+import { User } from '../../../typeorm/User.entity';
+import { Token } from '../../types/Token.type';
+import { RefreshAuthGuard } from '../../guards/RefreshAuthGuard.guard';
+import { FastifyRequest } from 'fastify';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  async login(@Request() req): Promise<Token> {
+    return this.authService.login(req.user);
+  }
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(
+    @Body() registerUserDto: CreateUserDto,
+  ): Promise<Partial<User>> {
+    return this.authService.register(registerUserDto);
+  }
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(RefreshAuthGuard)
+  async refreshToken(@Request() req: FastifyRequest): Promise<string> {
+    return this.authService.refreshTOken(req.user);
+  }
+}
--- src/auth/types/UserData.type.ts
+++ src/auth/types/UserData.type.ts
@@ -2,5 +2,4 @@ export interface UserData {
   email: string;
   username: string;
   password: string;
-  refreshToken: string;
 }
