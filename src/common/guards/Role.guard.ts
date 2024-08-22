7982afb5510f8f09f4a9095ce413ae22536f34c1
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-22 21:23:41 +0200

    feat: global role guard with a decorator

--- src/common/guards/Role.guard.ts
+++ src/common/guards/Role.guard.ts
@@ -1,16 +1,24 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/role.decorator';
 
 @Injectable()
-export class OrganizerGuard implements CanActivate {
+export class RoleGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const user = request.user;
-    if (user.role === 'Organizer') {
+    const roles = this.reflector.get<string[]>(ROLES_KEY, context.getHandler());
+    if (!roles) {
       return true;
     }
-    return false;
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user || !user.role) {
+      return false;
+    }
+
+    return roles.includes(user.role);
   }
 }
